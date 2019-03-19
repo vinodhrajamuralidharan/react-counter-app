51a7feebeb1484ca6da9b15ea32cf65450d9a830
vinodhraja muralidharan <vinodhraja.muralidharan@gmail.com>
2019-03-19 05:32:07 +0000

    events with param

--- src/counter.js
+++ src/counter.js
@@ -3,29 +3,31 @@ import React, {Component} from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ['tag1', 'tag2', 'tag3', 'tag4']
+    tags: []
   };
+
+  tagsHandler() {
+    if (this.state.tags.length) {
+      return  <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}  </ul>;
+    }
+
+    return 'There are no tags!'
+  }
+  incrementHandler= (val) => {
+    this.setState({
+      count: this.state.count+val
+    });
+    console.log('Increment clicked' + this.state.count);
+  }
   render() {
     return (
       <React.Fragment>
-        <span className={this.badgeClassHandler()}> {this.formatCount()} </span>
-        <button className="btn btn-primary">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        <span className="badge badge-primary">{this.state.count}</span>
+        {this.state.tags.length === 0 && 'Please enter some tags'}
+        {this.tagsHandler()}
+        <button className="btn btn-primary" onClick={() => this.incrementHandler(1)}>Increment</button>
       </React.Fragment>
     );
-  }
-
-  formatCount() {
-    const {count} = this.state;
-    return count === 0 ? 'zero' : count;
-  }
-  badgeClassHandler() {
-    const {count} = this.state;
-    let className = 'badge m-2 badge-';
-    className += count === 0 ? 'warning' : 'primary';
-    return className;
   }
 }
 
